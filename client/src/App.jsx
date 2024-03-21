2e33fbfa46c33c3f36a06b0cef2ff215b98bc655
samad <yy5841453@gmail.com>
2024-03-21 14:06:17 +0600

    create pages and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,44 @@
-import { useState } from 'react'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Project from './pages/Project';
+import Dashboard from './pages/Dashboard';
+const router =createBrowserRouter([
+  {
+    path : '/',
+    element : <Home/>
+  },
+  {
+    path : '/about',
+    element : <About/>
+  },
+  {
+    path : '/sign-in',
+    element : <SignIn/>
+  },
+  {
+    path : '/sign-up',
+    element : <SignUp/>
+  },
+  {
+    path : '/project',
+    element : <Project/>
+  },
+  {
+    path : '/dashboard',
+    element : <Dashboard/>
+  },
+])
 
 
 function App() {
-
-
   return (
     <>
-     <h1>hello</h1>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
