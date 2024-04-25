20e731da633f9cc6af4d5ec1b801e4a4a31e6eae
samad <yy5841453@gmail.com>
2024-04-25 13:37:01 +0600

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express'
+import 'dotenv/config'
+import mongoose from 'mongoose'
+
+
+mongoose.connect(process.env.MONGO).then(() => {
+  console.log('MongoDb is connected');
+})
+.catch((err) => {
+  console.log(err);
+});
 
 const app = express()
 
--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 function About() {
+  useEffect(() => {
+    const handleMouseLeave = (event) => {
+      event.preventDefault();
+      event.returnValue = ''; // Some browsers require a return value to trigger the confirmation message
+      return ''; // For compatibility with some older browsers
+    };
+
+    window.addEventListener('mouseleave', handleMouseLeave);
+
+    return () => {
+      window.removeEventListener('mouseleave', handleMouseLeave);
+    };
+  }, []);
+
   return (
-    <div>About</div>
-  )
+    <div>
+      <h1>Your Website Content</h1>
+      <p>This is your website content.</p>
+    </div>
+  );
 }
 
 export default About
